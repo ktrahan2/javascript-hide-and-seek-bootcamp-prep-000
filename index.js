40dc9fd84fdea1b905dff4889eb5fbd2911cffe1
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-14 23:29:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  let first = document.querySelector(selector)
+  return first
+}
 
+function nestedTarget() {
+  const second = document.getElementById('nested').getElementsByClassName('target')[0]
+  return second
+}
+
+function increaseRankBy(n) {
+  let rankedlist = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < rankedlist.length; i++) {
+    let children = rankedlist[i].children
+    for (let j = 0; j < children.length; j++) {
+      children[j].innerHTML = (parseInt(children[j].innerHTML) + n)
+    }
+  }
+}
